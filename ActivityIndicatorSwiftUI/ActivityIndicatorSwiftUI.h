cbae533a30491a1eedf1b26e16882d06c011fd5c
Łukasz Gryl <lukasz.gryl@gmail.com>
2020-10-26 06:48:49 +0100

    Initial commit

---
+++ ActivityIndicatorSwiftUI/ActivityIndicatorSwiftUI.h
@@ -0,0 +1,13 @@
+// Created 27/10/2020
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ActivityIndicatorSwiftUI.
+FOUNDATION_EXPORT double ActivityIndicatorSwiftUIVersionNumber;
+
+//! Project version string for ActivityIndicatorSwiftUI.
+FOUNDATION_EXPORT const unsigned char ActivityIndicatorSwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActivityIndicatorSwiftUI/PublicHeader.h>
+
+
---
+++ ActivityIndicatorUIKit/ActivityIndicatorUIKit.h
@@ -0,0 +1,13 @@
+// Created 27/10/2020
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ActivityIndicatorUIKit.
+FOUNDATION_EXPORT double ActivityIndicatorUIKitVersionNumber;
+
+//! Project version string for ActivityIndicatorUIKit.
+FOUNDATION_EXPORT const unsigned char ActivityIndicatorUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActivityIndicatorUIKit/PublicHeader.h>
+
+
